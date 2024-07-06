48a7b036ab3ffb5b6b99aa347803a4526b8fb7d0
Nathan <urwin.nathan@gmail.com>
2024-07-06 14:03:42 +0100

    Add watchfiles to devserver

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
 	entry: "./src/index.js",
 	devtool: "source-map",
 	devServer: {
-		static: "./dist",
+		watchFiles: ["./src/index.js", "./src/index.html"],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
