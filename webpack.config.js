1a1b5613d7beccaeeb36c0a729a7442f7d5ebe5d
Nathan <urwin.nathan@gmail.com>
2024-07-06 13:55:50 +0100

    Fix devtool case, set to prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,9 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+	mode: "production",
 	entry: "./src/index.js",
-	devTool: "source-map",
+	devtool: "source-map",
 	devServer: {
 		static: "./dist",
 	},
