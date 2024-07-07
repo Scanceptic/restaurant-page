1ba7b32abb7e24d8da3c8515e060b0f293b8c49e
Nathan <urwin.nathan@gmail.com>
2024-07-07 09:30:52 +0100

    add comments to pageload.js and initial clear

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import pageload from "./pageload.js";
 import "./style.css";
 
 pageload();
+
+/* Write Tab-switching logic under here */
+/* 
+    Event listener for each button in the navbar
+    wipe out the current content of div#content (while(firstchild) get firstchild remove firstchild)
+    Run the tab module (home.js, menu.js, about.js) to populate div#content with the new tab
+*/
--- src/pageload.js
+++ src/pageload.js
@@ -1,47 +1,50 @@
+/* 
+	Homepage module - first page that loads on visiting the site
+	also accessed through home button on navbar
+*/
+
 import Image from "./bistro.jpg";
 
 export default function pageload() {
+	// select div#content
 	const content = document.querySelector("#content");
-
+	// wipe out current contents
+	while (content.firstChild) {
+		content.removeChild(content.firstChild);
+	}
+	// create headline
 	const headline_div = document.createElement("div");
 	headline_div.id = "headline";
 	const headline = document.createElement("h1");
 	headline.textContent = "Larry's Lobster Bistro";
 	headline_div.appendChild(headline);
-
+	// create image column
 	const col1 = document.createElement("div");
 	col1.classList.add("col");
 	col1.id = "col1";
-
+	// create text column
 	const col2 = document.createElement("div");
 	col2.classList.add("col");
 	col2.id = "col2";
-
+	// attempt image link
 	try {
 		const myIcon = document.createElement("img");
 		myIcon.src = Image;
 		myIcon.alt = "Mozzarella and Tomato on a baguette slice";
 		col1.appendChild(myIcon);
 	} catch {
-		/* const myImage = document.createElement("img");
-			myImage.src =
-				"https://images.unsplash.com/photo-1708980108318-4b843e243080";
-			myImage.alt = "Mozzarella and Tomato on a baguette slice";
-			col1.appendChild(myImage);
-        */
-
 		console.log("Image load failed");
 	}
-
+	// add attribution to image
 	const attribution = document.createElement("p");
 	attribution.innerHTML = `Photo by <a href="https://unsplash.com/@vlado095?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Vlado Chabal</a> on <a href="https://unsplash.com/photos/a-piece-of-bread-topped-with-cheese-and-vegetables-nsaEvkk7d1g?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>`;
 	col1.appendChild(attribution);
-
+	// add text to text column (col2)
 	const restaurant_text = document.createElement("p");
 	restaurant_text.textContent =
 		"Larry's Bistro is the best place to go for premium crab, top-quality lobster, and a whole lot more!";
 	col2.appendChild(restaurant_text);
-
+	// append all the elements to content
 	const container = document.createElement("div");
 	container.classList.add("container");
 	container.appendChild(col1);
